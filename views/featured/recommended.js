c8bc67e2ee4cc9beb02406462c57a6411432be82
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-29 14:37:02 +0800

    Add empty component for flat list
    but due to limitation in react-native, flex will not work because there is an extra wrapper inside the list. This is a known issue.

--- views/featured/recommended.js
+++ views/featured/recommended.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View, Text } from 'react-native';
 import axios from 'axios';
 
 import ShopCard from './shop-card';
+
+const EmptyList = () => (
+  <View>
+    <Text
+      style={{
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: 'lightgrey',
+        fontSize: 20
+      }}>
+      Oops, we couldn't get any restaurants
+    </Text>
+  </View>
+);
 
 class RecommendedShops extends Component {
   state = {
@@ -18,14 +32,21 @@ class RecommendedShops extends Component {
         data={this.state.shops}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <ShopCard data={item} />}
+        ListEmptyComponent={<EmptyList />}
       />
     );
   }
 
   componentDidMount() {
-    axios.get(this.state.api).then((res) => {
-      this.setState({ shops: res.data });
-    });
+    axios
+      .get(this.state.api)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ shops: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
