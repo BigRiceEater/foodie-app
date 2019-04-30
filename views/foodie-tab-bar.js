1fbbdacc0c4921b2cb3bda1a0af78d8ffc02272f
big.rice.eater <34292181+BigRiceEater@users.noreply.github.com>
2019-04-30 11:43:48 +0800

    Add border to tabbar

--- views/foodie-tab-bar.js
+++ views/foodie-tab-bar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import ViewOverflow from 'react-native-view-overflow';
 
 import MemberTabButton from './member-tab-button';
 
 const S = StyleSheet.create({
-  container: { flexDirection: 'row', height: 52, elevation: 2 },
-  tabButton: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+  container: {
+    flexDirection: 'row',
+    height: 52,
+    borderTopWidth: 1,
+    borderColor: 'tomato'
+  },
+  tabButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 const FoodieTabBar = (props) => {
@@ -25,32 +35,36 @@ const FoodieTabBar = (props) => {
 
   return (
     <SafeAreaView>
-      <View style={S.container}>
-        {routes.map((route, routeIndex) => {
-          const isRouteActive = routeIndex === activeRouteIndex;
-          const tintColor = isRouteActive ? activeTintColor : inactiveTintColor;
+      <ViewOverflow>
+        <View style={S.container} pointerEvents='box-none'>
+          {routes.map((route, routeIndex) => {
+            const isRouteActive = routeIndex === activeRouteIndex;
+            const tintColor = isRouteActive
+              ? activeTintColor
+              : inactiveTintColor;
 
-          return (
-            <TouchableOpacity
-              key={routeIndex}
-              style={S.tabButton}
-              onPress={() => {
-                onTabPress({ route });
-              }}
-              onLongPress={() => {
-                onTabLongPress({ route });
-              }}
-              accessibilityLabel={getAccessibilityLabel({ route })}>
-              {renderIcon({ route, focused: isRouteActive, tintColor })}
+            return (
+              <TouchableOpacity
+                key={routeIndex}
+                style={S.tabButton}
+                onPress={() => {
+                  onTabPress({ route });
+                }}
+                onLongPress={() => {
+                  onTabLongPress({ route });
+                }}
+                accessibilityLabel={getAccessibilityLabel({ route })}>
+                {renderIcon({ route, focused: isRouteActive, tintColor })}
 
-              <Text style={{ color: tintColor }}>
-                {getLabelText({ route })}
-              </Text>
-            </TouchableOpacity>
-          );
-        })}
-        <MemberTabButton navigation={navigation} />
-      </View>
+                <Text style={{ color: tintColor }}>
+                  {getLabelText({ route })}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+          <MemberTabButton navigation={navigation} />
+        </View>
+      </ViewOverflow>
     </SafeAreaView>
   );
 };
